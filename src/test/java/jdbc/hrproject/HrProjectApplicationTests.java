d83c21dfd9288222098c424ff0f45964d0f0a000
aghowinem <aghowinem@gmail.com>
2023-03-11 18:08:43 +0300

    Initial commit

---
+++ src/test/java/jdbc/hrproject/HrProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package jdbc.hrproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
